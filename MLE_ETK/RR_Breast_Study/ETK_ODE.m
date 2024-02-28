24c3544a3d93d2aca4caba0a0005f8bead2e1854
mconte93 <154325375+mconte93@users.noreply.github.com>
2024-02-28 21:18:26 +0100

    Add files via upload

---
+++ MLE_ETK/RR_Breast_Study/ETK_ODE.m
@@ -0,0 +1,16 @@
+function dydt = ETK_ODE(t,y,par,VIF,t_VIF)
+
+%States
+C=y; 
+
+%Parameters
+   ktr=par(1);
+   ve=par(2);
+   vp=par(3);
+
+%Equations
+VIF=interp1(t_VIF,VIF,t,'linear'); 
+
+dydt =ktr.*(VIF-(C./ve)); 
+
+end
---
+++ MLE_ETK/RR_Breast_Study/fun_ETK_PS_RR_Breast_vpPe.m
@@ -0,0 +1,35 @@
+function [MLE] = fun_ETK_PS_RR_Breast_vpPe(x)
+
+%Function to evaluate the MLE function in the optimization process with
+%vp parameter fix and persistent curve profile in the RR case
+
+load('RR_Breast_Forward_with_inverse_Persistent.mat','CA_XY_time','t_VIF')
+ydata=CA_XY_time;
+xdata=t_VIF;
+load('QIN-BREAST02-01.mat','VIF')
+
+global vp
+   
+   ktr=x(1);
+   ve=x(2);
+
+   %Vector parameter and initial conditions
+   par_temp=[ktr,ve,vp];
+   C_0=0;   
+   Init=C_0;
+
+   %Forward ETK model
+   [t,y] = ode45(@(t,y) ETK_ODE(t,y,par_temp,VIF,xdata),xdata,Init,[]);
+   C_approx=y+vp*VIF;
+
+   %MLE evalutation
+   MLE=0;
+   for i=1:size(C_approx,1)
+       res=ydata(i)-C_approx(i);
+       if ~isnan(res)
+          MLE=MLE+res.^2;
+       end
+   end
+
+    
+end
